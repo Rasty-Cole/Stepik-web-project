13d256dd1de8ddd3d5bf8e4faf2289b598914a62
Rasty-Cole <pestow.lexa@yandex.ru>
2018-07-19 04:52:57 +0700

    Step 1.9: Update hello.py file.

--- etc/hello.py
+++ etc/hello.py
@@ -1,18 +1,14 @@
-import gunicorn
+bind = '0.0.0.0:8080'
+pythonpath = '/home/box/web'
 
 def app(environ, start_response):
-    data = environ['QUERY_STRING']
-    lines = data.split('&')
-
     result = ""
-    for line in lines:
+    for line in environ['QUERY_STRING'].split('&'):
         result = result + line + "\n"
-#        if line != lines[-1]: result = result + "\n"
 
-    status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
         ('Content-Length', str(len(result)))
     ]
-    start_response(status, response_headers)
-    return iter([result])
+    start_response('200 OK', response_headers)
+    return iter([result.encode('utf-8')])
