b7170c87490d7dc61bbe165ce362829aff577621
Rasty-Cole <alescha.pestov@yandex.ru>
2018-08-31 00:46:15 +0700

    Step 2.3: Creating models in a django web project.

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import timedelta
 
-# Create your models here.
+class MyUser(User):
+    def __str__(self):
+        return self.username
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return Question.objects.order_by('-added_at')
+
+    def popular(self):
+        return Question.objects.order_by('-rating')
+
+class Question(models.Model):
+    manager = QuestionManager()
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    rating = models.IntegerField()
+    author = models.CharField(max_length=64)
+    likes = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.text
--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE dbqa"
+mysql -uroot -e "'dandi'@'localhost' IDENTIFIED BY 'dandi'"
 cd /home/box/web/ask
 gunicorn -b 0.0.0.0:8000 ask.wsgi
